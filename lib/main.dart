a1c3c236497c10e30c3e6c6633245b103e0ef752
Your Name~manojprabhakar24 <manojptn16@gmail.com>
2023-08-16 10:16:06 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( MyApp());
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Gill's Bio"),
+        ),
+        body: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.all(20),
+              width: double.infinity,
+              alignment: Alignment.center,
+              padding: EdgeInsets.all(10),
+              child: Text(
+                "Subhmam Gill Biography", style: TextStyle(fontSize: 25),),
+
+            ),
+            Image.asset('images/subhman.png',
+              width: 350,
+              height: 250,),
+            Padding(padding: EdgeInsets.only(bottom: 50)),
+
+            Text(
+                "Shubman Gill, the young Indian batsman’s top-notch performances received a lot of applause from a certain Sara Tendulkar as well. Over the past few months, there have been numerous rumors about Shubman and Sara dating each other."),
+
+            Padding(padding: EdgeInsets.only(bottom: 50)),
+            Text(
+                "Shubman Gill -the right-handed batsman and right-arm off-break bowler plays as an opening batsman for the team. Hailing from Punjab, Gill plays a vital role in India’s historic win at Gabba in 2020 test series.")
+          ],
+        ),
+      ),
+    );
+  }
+
+
+}
+
+
+
+
